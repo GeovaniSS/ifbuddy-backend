5c89266464ea153ca8dad559e8c25358e8f9f7e0
GeovaniSS <geosilvasousa@gmail.com>
2025-05-19 19:38:42 -0300

    feat: adicionado DTO de filtros

---
+++ src/main/java/br/com/ifbuddy/rest/dto/FiltrosDTO.java
@@ -0,0 +1,15 @@
+package br.com.ifbuddy.rest.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class FiltrosDTO {
+  private Long cursoId;
+  private String turno;
+  private Integer semestre;
+  private String tipoTCC;
+  private List<Long> temasIds;
+  private List<Long> pontosFortesIds;
+}
