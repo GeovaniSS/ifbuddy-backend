62946a6520a7d834be11ef502d5bc6b6cc54468d
GeovaniSS <geosilvasousa@gmail.com>
2025-05-24 16:18:53 -0300

    feat: adicionado método para criar endereço

--- src/main/java/br/com/ifbuddy/services/EnderecoService.java
+++ src/main/java/br/com/ifbuddy/services/EnderecoService.java
@@ -6,6 +6,7 @@ import br.com.ifbuddy.models.Endereco;
 import br.com.ifbuddy.repository.EnderecoRepository;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 
 @RequestScoped
 public class EnderecoService {
@@ -14,5 +15,15 @@ public class EnderecoService {
 
   public List<Endereco> listarEnderecos() {
     return enderecoRepository.listAll();
+  }
+
+  @Transactional
+  public Endereco criarEndereco(String uf, String cidade, String bairro) {
+    Endereco endereco = new Endereco();
+    endereco.setUf(uf);
+    endereco.setCidade(cidade);
+    endereco.setBairro(bairro);
+    enderecoRepository.persist(endereco);
+    return endereco;
   }
 }
