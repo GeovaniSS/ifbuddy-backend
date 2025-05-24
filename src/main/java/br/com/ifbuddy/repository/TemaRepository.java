067c85fda8dd2d80eac75037a4b5fedddd85c445
GeovaniSS <geosilvasousa@gmail.com>
2025-05-24 16:21:08 -0300

    feat: adicionado método para buscar temas por ids

--- src/main/java/br/com/ifbuddy/repository/TemaRepository.java
+++ src/main/java/br/com/ifbuddy/repository/TemaRepository.java
@@ -1,9 +1,17 @@
 package br.com.ifbuddy.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import br.com.ifbuddy.models.Tema;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class TemaRepository implements PanacheRepository<Tema> {
+  public Set<Tema> buscarTemasPorIds(List<Long> ids) {
+    return find("id in ?1", ids).list()
+        .stream()
+        .collect(java.util.stream.Collectors.toSet());  
+  }
 }
