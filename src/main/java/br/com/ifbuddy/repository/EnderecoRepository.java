a9f0ba76cd1cd19f9a63cf8072be8ab04c1e6a56
GeovaniSS <geosilvasousa@gmail.com>
2025-06-07 18:20:48 -0300

    removido entidade de Endereco

--- src/main/java/br/com/ifbuddy/repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ifbuddy.repository;
-
-import br.com.ifbuddy.models.Endereco;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class EnderecoRepository implements PanacheRepository<Endereco> {}
